662582cc6b89960368bfbc5e3fce2f02da1737f7
graemlin <amundy14@vt.edu>
2018-04-13 16:27:17 -0400

    Add files via upload

---
+++ main.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+
+
+public class main {
+
+	public static void main(String[] args){
+		
+		int numPoints; //number of points
+		int numQueries; //number of queries
+		
+		Scanner s = new Scanner(System.in);
+		
+		numPoints = s.nextInt();
+		numQueries = s.nextInt();
+		
+		node[] points = new node[numPoints];
+		query[] queries = new query[numQueries];
+		
+		int[] coords = new int[3];
+		for(int i = 0; i < numPoints; i++){
+			coords[0] = s.nextInt();
+			coords[1] = s.nextInt();
+			coords[2] = s.nextInt();
+			points[i] = new node(coords);
+		}
+		
+		coords = new int[6];
+		for(int i = 0; i < numQueries; i++){
+			coords[0] = s.nextInt();
+			coords[1] = s.nextInt();
+			coords[2] = s.nextInt();
+			coords[3] = s.nextInt();
+			coords[4] = s.nextInt();
+			coords[5] = s.nextInt();
+			queries[i] = new query(coords);
+		}
+		
+		
+	}
+}

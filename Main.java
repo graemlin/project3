6b199ded014e3068f3ca21c4a4b14daa6c3d53c2
mbhatia4336 <mbhatia4336@5478uVwX3257.local>
2018-04-13 19:01:00 -0400

    buildKDTree

--- Main.java
+++ Main.java
@@ -69,4 +69,74 @@ public class Main {
 			System.out.println(pointsByZ.get(i));
 		}
 	}
+
+	private static node buildKDTree(ArrayList<node> sortedX, ArrayList<node> sortedY, int depth) {
+
+		if(depth%2 == 0) {
+			if(sortedX.size() == 1) {
+				node leaf = sortedX.get(0);
+				leaf.rightChild = null;
+				leaf.leftChild = null;
+				return leaf;
+			}
+
+			int median = (int)Math.ceil(sortedX.size()/2);
+
+			//split on x with median on left
+			ArrayList<node> leftX = new ArrayList<>(sortedX.subList(0, median));
+			ArrayList<node> rightX = new ArrayList<>(sortedX.subList(median, sortedX.size()));
+			node splitNode = leftX.get(leftX.size()-1);
+
+			ArrayList<node> leftY = new ArrayList<>();
+			ArrayList<node> rightY = new ArrayList<>();
+
+			//select subset of nodes from sorted y list
+			for (int i=0; i<sortedY.size(); i++) {
+				if(sortedY.get(i).value[0] > splitNode.value[0]) {
+					rightY.add(sortedY.get(i));
+				} else {
+					leftY.add(sortedY.get(i));
+				}
+			}
+			node vLeft = buildKDTree(leftX, leftY, depth+1);
+			node vRight = buildKDTree(rightX, rightY, depth+1);
+
+			splitNode.leftChild = vLeft;
+			splitNode.rightChild = vRight;
+
+		} else {
+			if(sortedY.size() == 1) {
+				node leaf = sortedY.get(0);
+				leaf.rightChild = null;
+				leaf.leftChild = null;
+				return leaf;
+			}
+
+			int median = (int)Math.ceil(sortedY.size()/2);
+			//split on y with median on bottom
+			ArrayList<node> bottomY = new ArrayList<>(sortedY.subList(0, median));
+			ArrayList<node> topY = new ArrayList<>(sortedY.subList(median, sortedY.size()));
+			node splitNode = bottomY.get(bottomY.size()-1);
+
+			ArrayList<node> bottomX = new ArrayList<>();
+			ArrayList<node> topX = new ArrayList<>();
+
+			//select subset of nodes from sorted x list
+			for (int i=0; i<sortedX.size(); i++) {
+				if(sortedX.get(i).value[1] > splitNode.value[1]) {
+					topX.add(sortedX.get(i));
+				} else {
+					bottomX.add(sortedX.get(i));
+				}
+			}
+
+			node vLeft = buildKDTree(bottomX, bottomY, depth+1);
+			node vRight = buildKDTree(topX, topY, depth+1);
+
+			splitNode.leftChild = vLeft;
+			splitNode.rightChild = vRight;
+
+		}
+
+	}
 }

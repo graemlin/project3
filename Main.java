bb8d09f3578d35aa709903ad66ee479da0b0231d
awhite4 <aliyahwhite@mac.com>
2018-04-21 19:59:30 -0400

    Update Main.java

--- Main.java
+++ Main.java
@@ -15,14 +15,17 @@ public class Main {
 		numPoints = s.nextInt();
 		numQueries = s.nextInt();
 		
+		//create ArrayLists of nodes and queries
 		ArrayList<node> points = new ArrayList<node>();
 		ArrayList<query> queries = new ArrayList<query>();
 		
+		//add the x, y, and z values read in, and id value from the for loop, as a node to the node array
 		int[] coords = new int[3];
 		for(int i = 0; i < numPoints; i++){
 			points.add(new node(s.nextInt(),s.nextInt(), s.nextInt(), i));
 		}
 		
+		//add the queries read in to the query array
 		coords = new int[6];
 		for(int i = 0; i < numQueries; i++){
 			coords[0] = s.nextInt();
@@ -34,6 +37,7 @@ public class Main {
 			queries.add(new query(coords));
 		}
 		
+		//create an ArrayList of nodes that will then be sorted by their x, y, or z values
 		ArrayList<node> pointsByX = new ArrayList<node>(points);
 		ArrayList<node> pointsByY = new ArrayList<node>(points);
 		ArrayList<node> pointsByZ = new ArrayList<node>(points);
@@ -42,43 +46,17 @@ public class Main {
 		Collections.sort(pointsByY, new sortNodes('y'));
 		Collections.sort(pointsByZ, new sortNodes('z'));
 
-
+		//set the region of x and y by using the sorted nodes
 		Integer[] region = {pointsByX.get(0).value[0], pointsByX.get(pointsByX.size() -1).value[0],
 				pointsByY.get(0).value[1], pointsByY.get(pointsByY.size() -1).value[1]};
 
+		//define the root node
 		node root = KDTree.buildKDTree(pointsByX,pointsByY,pointsByZ,region,0);
-		long totalSum = 0;
+		
+		//for each of the queries, seach the KDTree to get the number of points within that range and print it out
 		for(int i = 0; i < numQueries; i++){
 			int result = KDTree.searchKDTree(root, queries.get(i)).size();
 			System.out.println(result);
-			totalSum += result;
 		}
-		//System.out.println(totalSum);
-
-		//test code
-		/*System.out.println("points test");
-		for(int i = 0; i < numPoints; i++){
-			System.out.println(points.get(i));
-		}
-		
-		System.out.println("query test");
-		for(int i = 0; i < numQueries; i++){
-			System.out.println(queries.get(i));
-		}
-		
-		System.out.println("pointsByX test");
-		for(int i = 0; i < numPoints; i++){
-			System.out.println(pointsByX.get(i));
-		}
-		
-		System.out.println("pointsByY test");
-		for(int i = 0; i < numPoints; i++){
-			System.out.println(pointsByY.get(i));
-		}
-		
-		System.out.println("pointsByZ test");
-		for(int i = 0; i < numPoints; i++){
-			System.out.println(pointsByZ.get(i));
-		}*/
 	}
 }
